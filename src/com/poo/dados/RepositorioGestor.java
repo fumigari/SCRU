5445552913ec060df94b111daa6c17af3b2961be
Thales <thales.sous@outlook.com>
2015-11-26 22:10:35 -0200

    Commit 7

--- src/com/poo/dados/RepositorioGestor.java
+++ src/com/poo/dados/RepositorioGestor.java
@@ -1,5 +1,133 @@
 package com.poo.dados;
 
-public class RepositorioGestor {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
 
+
+import com.poo.excecoes.CadastroGestorExistenteException;
+import com.poo.negocios.beans.Gestor;
+
+public class RepositorioGestor implements IRepositorioGestor, Serializable{
+	private ArrayList<Gestor> listaDeGestores;
+	
+	private static RepositorioGestor instance;
+	
+	public RepositorioGestor(){
+		this.listaDeGestores = new ArrayList<Gestor>();
+	}
+	
+	public static IRepositorioGestor getInstance() throws IOException {
+		if (instance == null) {
+			instance = abrirArquivo();
+		}
+		return instance;
+	}
+	
+	private static RepositorioGestor abrirArquivo() throws IOException {
+
+		RepositorioAluno instanciaLocal = null;
+		File in = new File("ARQUIVOS\\CADASTRO ALUNOS\\cadastroalunos.bin");
+		FileInputStream fis = null;
+		ObjectInputStream ois = null;
+		try {
+			fis = new FileInputStream(in);
+			ois = new ObjectInputStream(fis);
+			Object o = ois.readObject();
+			instanciaLocal = (RepositorioGestor) o;
+		} catch (Exception e) {
+			
+			instanciaLocal = new RepositorioGestor();
+			
+		} finally {
+
+			if (ois != null) {
+				try {
+					ois.close();
+				} catch (IOException e) {/* Silent exception */
+				}
+			}
+		}
+
+		return instanciaLocal;
+
+	}
+	
+	public static void salvarArquivo() throws IOException {
+
+		if (instance == null) {
+			return;
+		}
+
+		File dir = new File("ARQUIVOS\\CADASTRO GESTORES");
+		dir.mkdirs();
+		File out = new File(dir,"cadastrogestores.bin");
+        
+		if (!out.exists()){
+			
+			out.createNewFile();
+        }
+		
+		FileOutputStream fos = null;
+		ObjectOutputStream oos = null;
+
+		try {
+			fos = new FileOutputStream(out);
+			oos = new ObjectOutputStream(fos);
+			oos.writeObject(instance);
+
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			
+		} finally {
+			if (oos != null) {
+				try {
+					oos.close();
+				} catch (IOException e) {/* Silent */
+				}
+			}
+		}
+	}
+	
+	//Existe baseado no CPF.
+	public boolean existe(Gestor gestor){
+		boolean achou = false;
+		for(int i = 0; i<this.listaDeGestores.size();i++){
+			if(this.listaDeGestores.get(i).getCpf().equals(gestor.getCpf())){
+				achou = true;
+			}
+		}
+		return achou;
+	}
+	
+	
+	public void inserirGestor(Gestor gestores) throws IOException, CadastroGestorExistenteException{
+		if(!this.existe(gestor)){
+			this.listaDeGestores.add(gestor);
+			salvarArquivo();
+		}else{
+			throw new CadastroGestorExistenteException();
+		}
+	}
+	
+	public ArrayList<Aluno> listarGestores(){
+		return this.listaDeGestores;
+	}
+	
+	
+
+	@Override
+	public boolean equals(Gestor a, Gestor B) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+	
 }
